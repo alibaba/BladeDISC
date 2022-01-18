a006cd210c11080be39e082b93c9853740af0f9a
tianyou.gty <tianyou.gty@alibaba-inc.com>
2022-01-18 10:58:23 +0800

    fix

--- scripts/ci/test_pytorch_blade.sh
+++ scripts/ci/test_pytorch_blade.sh
@@ -29,9 +29,9 @@ export TF_REMOTE_CACHE=${TF_REMOTE_CACHE}
 # note(yancey.yx): using virtualenv to avoid permission issue on workflow actions CI,
 python -m virtualenv venv && source venv/bin/activate
 
-export TORCH_BLADE_TORCH_VERSION=${TORCH_BLADE_TORCH_VERSION:-1.7.1+cu110}
+export TORCH_BLADE_CI_BUILD_TORCH_VERSION=${TORCH_BLADE_CI_BUILD_TORCH_VERSION:-1.7.1+cu110}
 (cd pytorch_blade \
-  && python -m pip install -q -r requirements-dev-${TORCH_BLADE_TORCH_VERSION}.txt \
+  && python -m pip install -q -r requirements-dev-${TORCH_BLADE_CI_BUILD_TORCH_VERSION}.txt \
        -f https://pai-blade.oss-cn-zhangjiakou.aliyuncs.com/pytorch/wheels/repo.html \
   && TORCH_LIB=$(python -c 'import torch; import os; print(os.path.dirname(os.path.abspath(torch.__file__)) + "/lib/")') \
   && export LD_LIBRARY_PATH=$TORCH_LIB:$LD_LIBRARY_PATH \

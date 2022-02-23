55567219971f380ea56ff6beceb45d3b3e7a0fc8
Yancey1989 <yancey.yx@antgroup.com>
2022-02-23 16:02:29 +0800

    remove unused code

--- docker/scripts/install-python.sh
+++ docker/scripts/install-python.sh
@@ -15,21 +15,6 @@ set -ex
 python3 -m pip install virtualenv numpy
 DISC_VENV=/opt/venv_disc
 
-function install_tf115() {
-  # install virtualenv: Python3+TensorFlow-GPU 1.15
-  pip install -q tensorflow-gpu==1.15
-}
-
-function install_tf24() {
-  # install virtualenv: Python3+TensorFlow-GPU 2.4.0
-  pip install -q tensorflow-gpu==2.4
-}
-
-function install_tf115_cpu() {
-  # install virtualenv: Python3+TensorFlow 1.15.5
-  pip install -q tensorflow==1.15.5
-}
-
 function install_venv() {
   python3 -m virtualenv ${DISC_VENV}
   source ${DISC_VENV}/bin/activate
@@ -41,18 +26,6 @@ function install_venv() {
     exit 1
   fi
 
-  if [[ ! -z "${BLADE_DISC_BUILT_CPU}" ]]; then
-    install_tf115_cpu
-  elif [[ "$CUDA_VERSION" == 10.0.* ]]; then
-    # CUDA10 Docker image
-    install_tf115
-  elif [[ "$CUDA_VERSION" == 11.0.* ]]; then
-    install_tf24
-    # CUDA11 Docker image
-  else
-    echo "unsupported CUDA version: " $CUDA_VERSION
-    exit -1
-  fi
   pip install pytest pytest-forked
   deactivate
 }

52c66ea51abf0b3e8b892d0d8a24b3093df57de4
Tanyo Kwok <tianyou.gty@alibaba-inc.com>
2022-02-24 18:15:24 +0800

    fix

--- scripts/ci/deploy_pytorch_blade.sh
+++ scripts/ci/deploy_pytorch_blade.sh
@@ -17,7 +17,9 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/deploy_docker.sh
 
 # build runtime Docker
-docker build -t ${REMOTE_RUNTIME_DOCKER} -f docker/runtime/Dockerfile.pytorch .
+docker build -t ${REMOTE_RUNTIME_DOCKER} -f docker/runtime/Dockerfile.pytorch \
+  --build-arg BASEIMAGE=${RUNTIME_BASEIMAGE} .
+
 
 # push TorchBlade runtime Docker images
 push_images

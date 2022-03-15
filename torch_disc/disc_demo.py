0cfccb96d96ed428b3930d0eedc9c2a3c2ed2385
Yan Xu <yancey1989@gmail.com>
2022-03-15 11:02:46 +0800

    [TorchDISC] setup torch-disc building environment and CI job (#158)
    This PR builds the TorchDISC pybind library  `_torch_disc.so` with Torch LTC, Torch Blade, and official PyTorch source code.
    And also add a CI job to test it with a rough way: `import _torch_disc`.
    
    The CI job includes two parts to reduce the CI time:
    1. building devel docker: install PyTorch with `lazy_tensor_staging` branch from source code.
    2. build and test torch-disc with the devel image from step1.
    
    Develops can build from zero with the guide https://github.com/alibaba/BladeDISC/blob/de3a386114dfcd6038c16c9fbe0737c0a6c5f6ef/torch_disc/README.md

---
+++ torch_disc/disc_demo.py
@@ -0,0 +1,59 @@
+# Copyright 2022 The BladeDISC Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+import _torch_disc as disc
+disc._ltc_init_disc_backend()
+
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+        self.fc = nn.Linear(784, 10)
+
+    def forward(self, x):
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+device = 'lazy'
+model = SimpleNet().to(device)
+
+transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+
+ds = datasets.MNIST('../data', train=True, download=True,
+                    transform=transform)
+
+train_loader = torch.utils.data.DataLoader(ds, batch_size=32, num_workers=1, pin_memory=True, shuffle=True)
+
+optimizer = optim.Adadelta(model.parameters(), lr=1.0)
+
+scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
+
+model.train().to(device)
+
+data, target = next(iter(train_loader))
+data, target = data.to(device), target.to(device)
+
+optimizer.zero_grad()
+output = model(data)
+loss = F.nll_loss(output, target)
+loss.backward()
+optimizer.step()
+#disc.mark_step()

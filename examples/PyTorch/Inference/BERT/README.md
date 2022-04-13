a727ddb054a8c177694750a67db2a3c391d7a84e
ZHENG, Zhen <james.zz@alibaba-inc.com>
2022-04-13 20:57:38 +0800

    Refactor for BERT example.

--- examples/PyTorch/Inference/BERT/README.md
+++ examples/PyTorch/Inference/BERT/README.md
@@ -1,14 +1,39 @@
-This is only a draft of the readme file. To rewrite it.
+# DISC optimization example for PyTorch BERT-large model.
 
-How to run:
+This repository provides a script showing how to optimize a BERT large inference
+model for PyTorch with DISC. The BERT-large model is constructed with Hugging
+Face API, with fake parameter values. It prepares fake input data for execution.
+
+
+## Configure and run.
+
+These python packages are required for executing this example:
+
+- torch >= 1.6.0
+- transformers
+- torch_blade
+
+To build and install `torch_blade` package, please refer to
+["Installation of TorchBlade"](../build_from_source.md) and
+["Install BladeDISC With Docker"](../install_with_docker.md).
+
+You can run the example with the following command:
+
 ```bash
 python main.py
 ```
 
-Requirements:
-- BladeDISC
-- Torch
-- TensorFlow
-- TensorRT
+By configuring `optimize_config` as `'DISC'`, or `None` in the script,
+you can run the model with DISC or without any optimization.
 
-You also need to install volksdep library to run TRT.
+
+## Performance results.
+
+We evaluate this example on T4 GPU, The CUDA version is 11.0. CuDNN version is 8.2.
+PyTorch version is 1.7.1. The average execution time of the 100 inferences is as following.
+
+| PyTorch |    DISC    |
+|-------------|-----------|
+|      |   |
+
+DISC shows a TBDx speedup over basic PyTorch.
--- examples/PyTorch/Inference/BERT/main.py
+++ examples/PyTorch/Inference/BERT/main.py
@@ -2,12 +2,10 @@
 import os
 
 os.environ["TORCH_BLADE_DEBUG_LOG"] = "off"
-#os.environ["TRT_LOG_LEVEL"] = "INFO"
+# Enable stitch fusion optimization.
 os.environ["DISC_ENABLE_STITCH"] = "true"
 os.environ["DISC_EXPERIMENTAL_SPECULATION_TLP_ENHANCE"] = "true"
-#os.environ["DEBUG_MAX_FUSION_NUMBERS"] = "196"
-#os.environ["TF_CPP_VMODULE"] = "disc_compiler=1"
-#os.environ["TAO_CPP_VMODULE"] = "disc_compiler=1"
+
 
 import time
 import ctypes
@@ -15,12 +13,10 @@ import numpy as np
 import torch
 import torch.onnx
 from transformers import BertModel, BertConfig, TFBertModel
-import tensorflow.compat.v1 as tf
-from tensorflow.python.saved_model import signature_constants, constants
-from tensorflow.core.protobuf import meta_graph_pb2
 
 import torch_blade
 
+# Tools for profiling, to be removed in the final release.
 _cudart = ctypes.CDLL('libcudart.so')
 def cu_prof_start():
     ret = _cudart.cudaProfilerStart()
@@ -80,25 +76,10 @@ def get_torch_bert_model(num_hidden_layers : int = 12, num_attention_heads : int
     return model
 
 
-def export_torch_to_onnx(model, output_file : str):
-    inputs = get_torch_test_data(cuda = True)
-    input_names = ['input_ids', 'attention_mask', 'token_type_ids']
-    output_names = ['last_hidden_state', 'pooler_output']
-    dynamic_axes={'input_ids' : {0 : 'batch_size', 1 : 'seq_length'},
-                  'attention_mask' : {0 : 'batch_size', 1 : 'seq_length'},
-                  'token_type_ids' : {0 : 'batch_size', 1 : 'seq_length'},
-                  'last_hidden_state' : {0 : 'batch_size', 1 : 'seq_length'},
-                  'pooler_output' : {0 : 'batch_size'}
-                  }
-    torch.onnx.export(model, tuple(inputs), output_file, opset_version=10,
-                      input_names = input_names, output_names = output_names,
-                      dynamic_axes = dynamic_axes)
-
-
-def print_torch_jit(model):
-    inputs = get_torch_test_data(cuda = True)
-    traced_model = torch.jit.trace(model.cuda(), inputs, strict=False)
-    # print(traced_model.graph)
+def get_torch_bert_large_model(amp : bool = False):
+    model = get_torch_bert_model(num_hidden_layers = 24, num_attention_heads = 16,
+                         hidden_size = 1024, amp = amp)
+    return model
 
 
 def save_torch_jit(model, file_name : str):
@@ -159,6 +140,21 @@ def run_torch_script(script_file : str, batch : int = 1 , seq_len : int = 64, in
     # print(model(*tuple(inputs)))
 
 
+def export_torch_to_onnx(model, output_file : str):
+    inputs = get_torch_test_data(cuda = True)
+    input_names = ['input_ids', 'attention_mask', 'token_type_ids']
+    output_names = ['last_hidden_state', 'pooler_output']
+    dynamic_axes={'input_ids' : {0 : 'batch_size', 1 : 'seq_length'},
+                  'attention_mask' : {0 : 'batch_size', 1 : 'seq_length'},
+                  'token_type_ids' : {0 : 'batch_size', 1 : 'seq_length'},
+                  'last_hidden_state' : {0 : 'batch_size', 1 : 'seq_length'},
+                  'pooler_output' : {0 : 'batch_size'}
+                  }
+    torch.onnx.export(model, tuple(inputs), output_file, opset_version=10,
+                      input_names = input_names, output_names = output_names,
+                      dynamic_axes = dynamic_axes)
+
+
 def save_trt_engine(onnx_file : str, trt_engine_name : str, opt_shape : list,
         min_shape : list = None, max_shape : list = None, fp16 : bool = False, int8 : bool = False):
     cmd = f'trtexec --onnx={onnx_file} --saveEngine={trt_engine_name} --workspace=256'
@@ -213,125 +209,6 @@ def run_trt_engine(trt_engine_name : str, batch : int = 1, seq_len : int = 64):
     # profile end
 
 
-def print_onnx(model_file : str):
-    import onnx
-
-    # Load the ONNX model
-    model = onnx.load(model_file)
-
-    # Check that the IR is well formed
-    onnx.checker.check_model(model)
-
-    # Print a human readable representation of the graph
-    print(onnx.helper.printable_graph(model.graph))
-
-
-def get_torch_bert_large_model(amp : bool = False):
-    model = get_torch_bert_model(num_hidden_layers = 24, num_attention_heads = 16,
-                         hidden_size = 1024, amp = amp)
-    return model
-
-
-def get_torch_bert_base_model(amp : bool = False):
-    model = get_torch_bert_model(num_hidden_layers = 12, num_attention_heads = 12,
-                         hidden_size = 768, amp = amp)
-    return model
-
-
-def run_torch_model(model, batch : int = 1, seq_len : int = 64):
-    inputs = get_torch_test_data(batch, seq_len, cuda = True)
-    model(*tuple(inputs))
-
-
-def get_tf_test_data(batch : int = 1, seq_len : int = 64, cuda : bool = False):
-    inp0 = np.zeros([batch, seq_len], dtype=int)
-    inp1 = np.zeros([batch, seq_len], dtype=int)
-    inp2 = np.zeros([batch, seq_len], dtype=int)
-    inputs = [inp0, inp1, inp2]
-    return input
-
-
-def get_tf_bert_model(num_hidden_layers : int = 12, num_attention_heads : int = 12,
-                      hidden_size : int = 768, amp : bool = False):
-    # Initializing a BERT bert-base-uncased style configuration
-    configuration = BertConfig()
-    configuration.num_hidden_layers = num_hidden_layers
-    configuration.num_attention_heads = num_attention_heads
-    configuration.hidden_size = hidden_size
-
-    model = TFBertModel(configuration)
-
-    return model
-
-
-def get_tf_bert_large_model(amp : bool = False):
-    model = get_tf_bert_model(num_hidden_layers = 24, num_attention_heads = 16,
-                         hidden_size = 1024, amp = amp)
-    return model
-
-
-def get_tf_bert_base_model(amp : bool = False):
-    model = get_tf_bert_model(num_hidden_layers = 12, num_attention_heads = 12,
-                         hidden_size = 768, amp = amp)
-    return model
-
-def load_tf_bert_model(path : str):
-    return tf.saved_model.load(path)
-
-
-def execute_tf_opt():
-  data = get_tf_test_data(batch = 1, seq_len = 64)
-  model = get_tf_bert_large_model()
-  tf.saved_model.save(model, 'saved')
-
-  test_data = {
-    "serving_default_attention_mask:0" : np.zeros([1, 64], dtype = int),
-    "serving_default_input_ids:0" : np.zeros([1, 64], dtype = int),
-    "serving_default_token_type_ids:0" : np.zeros([1, 64], dtype = int),
-  }
-
-  session_config = tf.ConfigProto()
-  session_config.log_device_placement = True
-  session_config.allow_soft_placement = True
-  #session_config.intra_op_parallelism_threads=int(16/process_num)
-  #session_config.inter_op_parallelism_threads=int(16/process_num)
-  session_config.gpu_options.allow_growth = True
-  session_config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
-  session_config.graph_options.rewrite_options.auto_mixed_precision = 1
-
-  with tf.Session(config=session_config) as sess:
-    meta_graph_def = tf.saved_model.loader.load(
-                    sess, [tf.saved_model.tag_constants.SERVING],
-                    './saved'
-    )
-    signature_def = meta_graph_def.signature_def[signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
-    inputs = signature_def.inputs
-    outputs = signature_def.outputs
-
-    fetch_dict = {}
-    for name, tensor in outputs.items():
-      fetch_dict[name] = tf.get_default_graph().get_tensor_by_name(tensor.name)
-
-    # warmup 
-    for i in range(50):
-      outs = sess.run(fetch_dict, feed_dict=test_data)
-
-    step_to_profile = 80
-    all_times = []
-    for i in range(100):
-      s = time.time()
-      # if i == step_to_profile:
-      #   cu_prof_start()
-      outs = sess.run(fetch_dict, feed_dict=test_data)
-      e = time.time()
-      # print(f'outs = {outs}')
-      # if i == step_to_profile:
-      #   cu_prof_stop()
-      # print(f'step #{i}: {e - s} s')
-      all_times.append(e - s)
-    print(f'mean: {np.mean(all_times)}, median: {np.median(all_times)}, max: {np.max(all_times)}, min: {np.min(all_times)}')
-
-
 def execute_torch_opt():
     batch = 1
     seq = 64
@@ -340,19 +217,16 @@ def execute_torch_opt():
     # BladeDISC optimization.
     save_torch_jit(bert_large_amp, 'bert_large_amp.pt')
     inputs = get_torch_test_data(batch = batch, seq_len = seq, cuda = True)
-    disc_optimize_torch('bert_large_amp.pt', 'bert_large_amp.stitch.pt')
-    run_torch_script('bert_large_amp.stitch.pt', batch = batch, seq_len = seq, in_data = inputs)
+    disc_optimize_torch('bert_large_amp.pt', 'bert_large_amp.opt.pt')
+    run_torch_script('bert_large_amp.opt.pt', batch = batch, seq_len = seq, in_data = inputs)
 
-    # TRT Optimization.
+    # TRT Optimization. To be removed in the final release.
     # To use TRT amp rather than Torch AMP.
     bert_large = get_torch_bert_large_model(amp = False)
     export_torch_to_onnx(bert_large, 'bert_large.onnx')
     save_trt_engine('bert_large.onnx', f'bert_large.fp16.{batch}x{seq}.trt', [batch, seq], fp16 = True)
     run_trt_engine(f'bert_large.fp16.{batch}x{seq}.trt', batch = batch, seq_len = seq)
-    # int8 does not work well.
-    #save_trt_engine('bert_large.onnx', f'bert_large.int8.{batch}x{seq}.trt', [batch, seq], fp16 = False, int8 = True)
 
 
 if __name__ == '__main__':
   execute_torch_opt()
-  #execute_tf_opt()

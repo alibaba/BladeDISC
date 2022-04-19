9dfcf28015d04497f650aacac30316ba93bd7b05
ZHENG, Zhen <james.zz@alibaba-inc.com>
2022-04-19 19:27:45 +0800

    PyTorch BERT model done.

--- examples/PyTorch/Inference/BERT/README.md
+++ examples/PyTorch/Inference/BERT/README.md
@@ -3,6 +3,7 @@
 This repository provides a script showing how to optimize a BERT large inference
 model for PyTorch with DISC. The BERT-large model is constructed with Hugging
 Face API, with fake parameter values. It prepares fake input data for execution.
+It also shows a performance comparison between BladeDISC and TensorRT.
 
 
 ## Configure and run.
@@ -12,10 +13,13 @@ These python packages are required for executing this example:
 - torch >= 1.6.0
 - transformers
 - torch_blade
+- volksdep
 
 To build and install `torch_blade` package, please refer to
 ["Installation of TorchBlade"](../build_from_source.md) and
 ["Install BladeDISC With Docker"](../install_with_docker.md).
+The [`volksdep`](https://github.com/Media-Smart/volksdep) library is used to
+execute the generated TensorRT engine.
 
 You can run the example with the following command:
 
@@ -23,17 +27,18 @@ You can run the example with the following command:
 python main.py
 ```
 
-By configuring `optimize_config` as `'DISC'`, or `None` in the script,
-you can run the model with DISC or without any optimization.
-
+It will run optimizations of BladeDISC and TensorRT.
 
 ## Performance results.
 
-We evaluate this example on T4 GPU, The CUDA version is 11.0. CuDNN version is 8.2.
-PyTorch version is 1.7.1. The average execution time of the 100 inferences is as following.
+We evaluate this example on T4 GPU, for which the clock is 1590 during the test.
+The CUDA version is 11.0. CuDNN version is 8.0. TensorRT version is 8.2. PyTorch
+version is 1.7.1. The average execution time of the 100 inferences is as
+following (ms).
 
-| PyTorch |    DISC    |
-|-------------|-----------|
-|      |   |
+| PyTorch | TRT static | Blade TRT static |  DISC  |
+|---------|------------|------------------|--------|
+|  40.77  |    4.56    |       17.01      |  4.71  |
 
-DISC shows a TBDx speedup over basic PyTorch.
+DISC shows a 8.66x speedup over basic PyTorch, and achieves similar performance
+with TensorRT static optimization.
--- examples/PyTorch/Inference/BERT/main.py
+++ examples/PyTorch/Inference/BERT/main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 import os
 
-os.environ["TORCH_BLADE_DEBUG_LOG"] = "off"
 # Enable stitch fusion optimization.
 os.environ["DISC_ENABLE_STITCH"] = "true"
 os.environ["DISC_EXPERIMENTAL_SPECULATION_TLP_ENHANCE"] = "true"
-
 
 import time
 import ctypes
@@ -15,20 +13,26 @@ import torch.onnx
 from transformers import BertModel, BertConfig, TFBertModel
 
 import torch_blade
+import torch_blade.tensorrt
 
 # Tools for profiling, to be removed in the final release.
 _cudart = ctypes.CDLL('libcudart.so')
+
+
 def cu_prof_start():
     ret = _cudart.cudaProfilerStart()
     if ret != 0:
         raise Exception('cudaProfilerStart() returned %d' % ret)
+
 
 def cu_prof_stop():
     ret = _cudart.cudaProfilerStop()
     if ret != 0:
         raise Exception('cudaProfilerStop() returned %d' % ret)
 
+
 class BertModelAMP(BertModel):
+
     def __init__(self, config, add_pooling_layer=True):
         with torch.cuda.amp.autocast():
             super().__init__(config, add_pooling_layer)
@@ -50,12 +54,17 @@ class BertModelAMP(BertModel):
         return_dict=None,
     ):
         with torch.cuda.amp.autocast():
-            return super().forward(input_ids, attention_mask, token_type_ids, position_ids, head_mask,
-                    inputs_embeds, encoder_hidden_states, encoder_attention_mask, past_key_values,
-                    use_cache, output_attentions, output_hidden_states, return_dict)
+            return super().forward(input_ids, attention_mask, token_type_ids,
+                                   position_ids, head_mask, inputs_embeds,
+                                   encoder_hidden_states,
+                                   encoder_attention_mask, past_key_values,
+                                   use_cache, output_attentions,
+                                   output_hidden_states, return_dict)
 
 
-def get_torch_test_data(batch : int = 1, seq_len : int = 128, cuda : bool = False):
+def get_torch_test_data(batch: int = 1,
+                        seq_len: int = 128,
+                        cuda: bool = False):
     inp0 = torch.zeros([batch, seq_len], dtype=torch.int).long()
     inp1 = torch.zeros([batch, seq_len], dtype=torch.int).long()
     inp2 = torch.zeros([batch, seq_len], dtype=torch.int).long()
@@ -65,68 +74,39 @@ def get_torch_test_data(batch : int = 1, seq_len : int = 128, cuda : bool = Fals
     return inputs
 
 
-def get_torch_bert_model(num_hidden_layers : int = 12, num_attention_heads : int = 12,
-                         hidden_size : int = 768, amp : bool = False):
-    configuration = BertConfig() # bert-base-uncased style configuration
+def get_torch_bert_model(num_hidden_layers: int = 12,
+                         num_attention_heads: int = 12,
+                         hidden_size: int = 768,
+                         amp: bool = False):
+    configuration = BertConfig()  # bert-base-uncased style configuration
     configuration.num_hidden_layers = num_hidden_layers
     configuration.num_attention_heads = num_attention_heads
     configuration.hidden_size = hidden_size
 
-    model = BertModelAMP(configuration).cuda().eval() if amp else BertModel(configuration).cuda().eval()
+    model = BertModelAMP(configuration).cuda().eval() if amp else BertModel(
+        configuration).cuda().eval()
     return model
 
 
-def get_torch_bert_large_model(amp : bool = False):
-    model = get_torch_bert_model(num_hidden_layers = 24, num_attention_heads = 16,
-                         hidden_size = 1024, amp = amp)
+def get_torch_bert_large_model(amp: bool = False):
+    model = get_torch_bert_model(num_hidden_layers=24,
+                                 num_attention_heads=16,
+                                 hidden_size=1024,
+                                 amp=amp)
     return model
 
 
-def save_torch_jit(model, file_name : str):
-    inputs = get_torch_test_data(cuda = True)
-    traced_model = torch.jit.trace(model.cuda(), inputs, strict=False)
-    torch.jit.save(traced_model, file_name)
-
-
-def disc_optimize_torch(in_file : str, out_file : str):
-    torch_config = torch_blade.config.Config()
-    torch_config.enable_mlir_amp = False # disable mix-precision
-
-    inputs = get_torch_test_data(cuda = True)
-    model = torch.jit.load(in_file).cuda().eval()
-
-    torch._C._jit_pass_inline(model._c.forward.graph)
-    torch._C._jit_pass_remove_dropout(model._c)
-
-    with torch.no_grad(), torch_config:
-      # BladeDISC torch_blade optimize will return an optimized TorchScript
-      optimized_ts = torch_blade.optimize(model, allow_tracing=True, model_inputs=tuple(inputs))
-    torch.jit.save(optimized_ts, out_file)
-
-
-def run_torch_script(model, batch : int = 1 , seq_len : int = 64, in_data = None):
-    if in_data is None: 
-        inputs = get_torch_test_data(batch, seq_len, cuda = True)
-    else:
-        inputs = in_data
-
+def evaluate_torch(model, inputs):
     # warmup
     for i in range(20):
         model(*tuple(inputs))
+
     iters = 100
     tic = time.time()
     for i in range(iters):
         model(*tuple(inputs))
     avg_time = (time.time() - tic) / iters
     print("average time in {} iterations: {} seconds".format(iters, avg_time))
-
-    exec_time = []
-    for i in range(50):
-        tic = time.time()
-        model(*tuple(inputs))
-        delta = time.time() - tic
-        exec_time.append(delta)
-    print("medium execution time of 50 iterations: {} seconds.".format(np.median(exec_time)))
 
     # profile start
     _cudart = ctypes.CDLL('/usr/local/cuda/lib64/libcudart.so')
@@ -136,23 +116,64 @@ def run_torch_script(model, batch : int = 1 , seq_len : int = 64, in_data = None
     # profile end
 
 
-def export_torch_to_onnx(model, output_file : str):
-    inputs = get_torch_test_data(cuda = True)
+def disc_optimize(model, inputs, out_file: str):
+    torch_config = torch_blade.config.Config()
+    torch_config.enable_mlir_amp = False  # disable mix-precision
+
+    traced_model = torch.jit.trace(model.cuda(), inputs,
+                                   strict=False).cuda().eval()
+
+    torch._C._jit_pass_inline(traced_model._c.forward.graph)
+    torch._C._jit_pass_remove_dropout(traced_model._c)
+
+    with torch.no_grad(), torch_config:
+        # BladeDISC torch_blade optimize will return an optimized TorchScript
+        optimized_ts = torch_blade.optimize(traced_model,
+                                            allow_tracing=True,
+                                            model_inputs=tuple(inputs))
+    torch.jit.save(optimized_ts, out_file)
+
+
+def export_torch_to_onnx(model, inputs, output_file: str):
     input_names = ['input_ids', 'attention_mask', 'token_type_ids']
     output_names = ['last_hidden_state', 'pooler_output']
-    dynamic_axes={'input_ids' : {0 : 'batch_size', 1 : 'seq_length'},
-                  'attention_mask' : {0 : 'batch_size', 1 : 'seq_length'},
-                  'token_type_ids' : {0 : 'batch_size', 1 : 'seq_length'},
-                  'last_hidden_state' : {0 : 'batch_size', 1 : 'seq_length'},
-                  'pooler_output' : {0 : 'batch_size'}
-                  }
-    torch.onnx.export(model, tuple(inputs), output_file, opset_version=10,
-                      input_names = input_names, output_names = output_names,
-                      dynamic_axes = dynamic_axes)
+    dynamic_axes = {
+        'input_ids': {
+            0: 'batch_size',
+            1: 'seq_length'
+        },
+        'attention_mask': {
+            0: 'batch_size',
+            1: 'seq_length'
+        },
+        'token_type_ids': {
+            0: 'batch_size',
+            1: 'seq_length'
+        },
+        'last_hidden_state': {
+            0: 'batch_size',
+            1: 'seq_length'
+        },
+        'pooler_output': {
+            0: 'batch_size'
+        }
+    }
+    torch.onnx.export(model,
+                      tuple(inputs),
+                      output_file,
+                      opset_version=10,
+                      input_names=input_names,
+                      output_names=output_names,
+                      dynamic_axes=dynamic_axes)
 
 
-def save_trt_engine(onnx_file : str, trt_engine_name : str, opt_shape : list,
-        min_shape : list = None, max_shape : list = None, fp16 : bool = False, int8 : bool = False):
+def save_trt_engine(onnx_file: str,
+                    trt_engine_name: str,
+                    opt_shape: list,
+                    min_shape: list = None,
+                    max_shape: list = None,
+                    fp16: bool = False,
+                    int8: bool = False):
     cmd = f'trtexec --onnx={onnx_file} --saveEngine={trt_engine_name} --workspace=256'
     type_str = 'fp16' if fp16 else 'fp32'
     if fp16:
@@ -173,29 +194,19 @@ def save_trt_engine(onnx_file : str, trt_engine_name : str, opt_shape : list,
         print(line)
 
 
-def run_trt_engine(trt_engine_name : str, batch : int = 1, seq_len : int = 64):
+def run_trt_engine(trt_engine_name: str, inputs):
     import volksdep.converters
     trt_model = volksdep.converters.load(trt_engine_name)
 
-    inputs = get_torch_test_data(batch, seq_len, cuda = True)
-
     # warmup
     for i in range(20):
-        trt_output = trt_model(inputs)
+        trt_model(inputs)
     iters = 100
     tic = time.time()
     for i in range(iters):
         trt_model(inputs)
     avg_time = (time.time() - tic) / iters
     print("average time in {} iterations: {} seconds".format(iters, avg_time))
-
-    exec_time = []
-    for i in range(50):
-        tic = time.time()
-        trt_model(inputs)
-        delta = time.time() - tic
-        exec_time.append(delta)
-    print("medium execution time of 50 iterations: {} seconds.".format(np.median(exec_time)))
 
     # profile start
     _cudart = ctypes.CDLL('/usr/local/cuda/lib64/libcudart.so')
@@ -205,29 +216,54 @@ def run_trt_engine(trt_engine_name : str, batch : int = 1, seq_len : int = 64):
     # profile end
 
 
-def run(optimize_config : str = None):
+def blade_trt_optimize(model, inputs, fp16: bool, out_file: str):
+    cfg = torch_blade.Config.get_current_context_or_new().clone()
+    cfg.optimization_pipeline = torch_blade.tensorrt.backend_name()
+    cfg.customize_onnx_opset_version = 12
+    cfg.enable_fp16 = fp16
+
+    traced_model = torch.jit.trace(model.cuda().eval(), inputs,
+                                   strict=False).cuda().eval()
+
+    with cfg, torch_blade.logging.logger_level_context('INFO'):
+        opt_model = torch_blade.optimize(traced_model,
+                                         False,
+                                         model_inputs=tuple(inputs))
+    torch.jit.save(opt_model, out_file)
+
+
+def run():
     batch = 1
     seq = 64
-    bert_large_amp = get_torch_bert_large_model(amp = True)
-    inputs = get_torch_test_data(batch = batch, seq_len = seq, cuda = True)
+    bert_large = get_torch_bert_large_model(amp=False)
+    bert_large_amp = get_torch_bert_large_model(amp=True)
+    inputs = get_torch_test_data(batch=batch, seq_len=seq, cuda=True)
 
+    # Run naive torch.
+    print("Naive PyTorch.")
     model = bert_large_amp
-    if optimize_config is 'disc':
-        # BladeDISC optimization.
-        save_torch_jit(bert_large_amp, 'bert_large_amp.pt')
-        disc_optimize_torch('bert_large_amp.pt', 'bert_large_amp.opt.pt')
-        model = torch.jit.load(script_file).cuda().eval()
-    run_torch(model, batch = batch, seq_len = seq, in_data = inputs)
+    evaluate_torch(model, inputs)
 
+    # Run BladeDISC optimization.
+    print("BladeDISC Optimization.")
+    disc_optimize(bert_large_amp, inputs, 'bert_large_amp.disc.pt')
+    model = torch.jit.load('bert_large_amp.disc.pt').cuda().eval()
+    evaluate_torch(model, inputs)
 
-    # TRT Optimization. To be removed in the final release.
-    # To use TRT amp rather than Torch AMP.
-    bert_large = get_torch_bert_large_model(amp = False)
-    export_torch_to_onnx(bert_large, 'bert_large.onnx')
-    save_trt_engine('bert_large.onnx', f'bert_large.fp16.{batch}x{seq}.trt', [batch, seq], fp16 = True)
-    run_trt_engine(f'bert_large.fp16.{batch}x{seq}.trt', batch = batch, seq_len = seq)
+    # Run TensorRT with `trtexec`. Static shape optimization.
+    print("Official TensorRT Static Optimization.")
+    export_torch_to_onnx(bert_large, inputs, 'bert_large.onnx')
+    save_trt_engine('bert_large.onnx',
+                    f'bert_large.fp16.{batch}x{seq}.trt', [batch, seq],
+                    fp16=True)
+    run_trt_engine(f'bert_large.fp16.{batch}x{seq}.trt', inputs)
+
+    # Run TorchBlade-TensorRT optimization.
+    print("TorchBlade-TensorRT Optimization.")
+    blade_trt_optimize(bert_large, inputs, True, 'bert_large_amp.trt.pt')
+    model = torch.jit.load('bert_large_amp.trt.pt').cuda().eval()
+    evaluate_torch(model, inputs)
 
 
 if __name__ == '__main__':
-    # `optimize_config` can be 'disc' or None.
-    run(optimize_config = 'disc')
+    run()

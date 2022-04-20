7abc266fa45b3c221e50f4645dee4743319045f4
tashuang.zk <tashuang.zk@alibaba-inc.com>
2022-04-20 17:03:24 +0800

    Add UnrealizedConversionCast for codegen of si/ui const

--- tao_compiler/mlir/disc/tests/regression/uint_cast.cc
+++ tao_compiler/mlir/disc/tests/regression/uint_cast.cc
@@ -34,4 +34,15 @@ TEST(UintCastTest, UintCastTest) {
       /*input_vals*/ {{255, 128, 0, 63, 158, 33}}));
 }
 
+TEST(TFConstOpTest, ScalarUI8) {
+  EXPECT_TRUE(feature_test_main(
+      /*mlir_file_path*/ c_ft_path + "uint_cast_const.mlir",
+      /*backend_types*/
+      kSupportedBackendList,
+      /*num_inputs*/ 1,
+      /*num_outputs*/ 1,
+      /*input_descriptors*/ {"4xui8_X"},
+      /*output_descriptors*/ {"f32_X"}));
+}
+
 }  // namespace mlir_test

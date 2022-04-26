3dd8a4ab6566196e538ae4f97eabdc94ff1ae55c
kevin.zwy <kevin.zwy@alibaba-inc.com>
2022-04-26 15:31:46 +0800

    fix

--- pytorch_blade/src/compiler/mlir/runtime/ral_context.cpp
+++ pytorch_blade/src/compiler/mlir/runtime/ral_context.cpp
@@ -215,11 +215,7 @@ at::List<at::Tensor> RalContext::CreateAndBindingOutputs(
       };
 #ifdef TORCH_BLADE_BUILD_WITH_CUDA
       if (output_info.device == "cuda") {
-        auto cuda_allocator = at::cuda::getCUDADeviceAllocator();
-        TORCH_CHECK(cuda_allocator != nullptr);
-        deleter = [cuda_allocator](void* ptr) {
-          cuda_allocator->raw_deallocate(ptr);
-        };
+        deleter = c10::cuda::CUDACachingAllocator::raw_delete;
       }
 #endif
       out_tensor = torch::from_blob(
@@ -249,15 +245,9 @@ tao::ral::BaseContext* RalContext::LoadCache() {
   tao::ral::gpu::BaseCudaContextOption gpu_opt;
   gpu_opt.device_ordinal = gpu_device_;
   gpu_opt.use_stream_executor = true;
-  auto torch_allocator = at::cuda::getCUDADeviceAllocator();
-  TORCH_CHECK(torch_allocator != nullptr);
-  auto cuda_alloc = [torch_allocator](size_t n) {
-    return torch_allocator->raw_allocate(n);
-  };
-  auto cuda_delete = [torch_allocator](void* ptr) {
-    torch_allocator->raw_deallocate(ptr);
-  };
-  gpu_opt.gpu_allocator.reset(new RalAllocator(cuda_alloc, cuda_delete));
+  gpu_opt.gpu_allocator.reset(new RalAllocator(
+      c10::cuda::CUDACachingAllocator::raw_alloc,
+      c10::cuda::CUDACachingAllocator::raw_delete));
 
   std::lock_guard<std::mutex> guard(mtx_);
   tao::ral::BaseContext* ral_ctx_ptr;

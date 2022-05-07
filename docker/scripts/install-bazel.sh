c2e19e07eb432d7932db05b07f206335de8e6343
ZHENG Zhen <zzchman@gmail.com>
2022-05-07 14:02:02 +0800

    Force update bazel.

--- docker/scripts/install-bazel.sh
+++ docker/scripts/install-bazel.sh
@@ -17,4 +17,4 @@ curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 apt-get update && apt-get install -y bazel-5.1.1
-ln -s /usr/bin/bazel-5.1.1 /usr/bin/bazel
+ln -sf /usr/bin/bazel-5.1.1 /usr/bin/bazel

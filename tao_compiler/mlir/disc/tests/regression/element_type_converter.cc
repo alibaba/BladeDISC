e2f348a43737ee384db438c81348fee81e1fee19
minmin.smm <minmin.smm@alibaba-inc.com>
2022-05-10 22:53:35 +0800

    add regrssion test

---
+++ tao_compiler/mlir/disc/tests/regression/element_type_converter.cc
@@ -0,0 +1,63 @@
+/* Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/compiler/mlir/disc/tests/mlir_feature_test.h"
+#include "tensorflow/compiler/mlir/disc/tests/mlir_test.h"
+#include "tensorflow/core/platform/test.h"
+
+namespace mlir_test {
+
+const std::string c_ft_path =
+    "tensorflow/compiler/mlir/disc/tests/regression/data/";
+
+TEST(ElementTypeConverterTest, Gemm) {
+  setenv("TAO_MLIR_ENABLE_AMP", "true", 1);
+  EXPECT_TRUE(feature_test_main(
+      /*mlir_file_path*/ c_ft_path + "onednn_gemm.mlir",
+      /*backend_types*/ {BackendType::kCuda},
+      /*num_inputs*/ 2,
+      /*num_outputs*/ 1,
+      /*input_descriptors*/ {"50x199xf32_X", "199x50xf32_X"},
+      /*output_descriptors*/ {"f32_X"},
+      /*input_vals*/ {}));
+  unsetenv("TAO_MLIR_ENABLE_AMP");
+}
+
+TEST(ElementTypeConverterTest, GemmWeightConst) {
+  setenv("TAO_MLIR_ENABLE_AMP", "true", 1);
+  EXPECT_TRUE(feature_test_main(
+      /*mlir_file_path*/ c_ft_path + "weight_prepacking_cpu_matmul.mlir",
+      /*backend_types*/ {BackendType::kCuda},
+      /*num_inputs*/ 1,
+      /*num_outputs*/ 1,
+      /*input_descriptors*/ {"100x110xf32_X"},
+      /*output_descriptors*/ {"f32_X"}));
+  unsetenv("TAO_MLIR_ENABLE_AMP");
+}
+
+TEST(ElementTypeConverterTest, ConvReluConvReluAddTest0) {
+  setenv("TAO_MLIR_ENABLE_AMP", "true", 1);
+  EXPECT_TRUE(feature_test_main(
+      /*mlir_file_path*/ c_ft_path + "transpose_simplifier_test0.mlir",
+      /*backend_types*/ {BackendType::kCuda},
+      /*num_inputs*/ 1,
+      /*num_outputs*/ 2,
+      /*input_descriptors*/ {"2x16x32x32xf32_X"},
+      /*output_descriptors*/ {"f32_X", "f32_X"},
+      /*input_vals*/ {}));
+  unsetenv("TAO_MLIR_ENABLE_AMP");
+}
+
+}  // namespace mlir_test

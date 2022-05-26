700372b536c01d5401b541697f0302d94a5ddc72
Yan Xu <yancey1989@gmail.com>
2022-05-26 09:33:52 +0800

    cuda device as an option flag for torch-disc backend (#338)
    * switch cuda as disc backend device
    
    * update
    
    * update

--- docker/scripts/install-bazel.sh
+++ docker/scripts/install-bazel.sh
@@ -11,10 +11,10 @@
 # limitations under the License.
 
 
-set -ex
+set -e
 apt-get install -y apt-transport-https curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
-apt-get update && apt-get install -y bazel-5.1.1
+apt-get update -y && apt-get install -y bazel-5.1.1
 ln -sf /usr/bin/bazel-5.1.1 /usr/bin/bazel

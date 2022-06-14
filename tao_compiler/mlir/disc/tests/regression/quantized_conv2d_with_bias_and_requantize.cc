31506badfe0175ca2222f2fe4fa16a5420b73653
Wenyi Zhao <kevin.zwy@alibaba-inc.com>
2022-06-14 19:43:25 +0800

    support quantized_conv2 with const zero bias with qint32 type (#389)
    * support quantized_conv2 with const zero bias with qint32 type
    
    * in case the value is empty tensor

--- tao_compiler/mlir/disc/tests/regression/quantized_conv2d_with_bias_and_requantize.cc
+++ tao_compiler/mlir/disc/tests/regression/quantized_conv2d_with_bias_and_requantize.cc
@@ -39,4 +39,22 @@ TEST(QuantizedConv2DWithBiasAndRequantizeOpTest, I8I8I8_NO_BIAS) {
       /*expect_output_vals*/ {output}));
 }
 
+TEST(QuantizedConv2DWithBiasAndRequantizeOpTest, I8I8I8_NO_BIAS_QINT32) {
+  std::vector<float> inputs(1 * 2 * 2 * 25, -0.6);
+  tensorflow::Tensor output(tensorflow::DataType::DT_FLOAT, {1, 2, 2, 8});
+  auto datas = output.flat<float>();
+  for (int i = 0; i < output.NumElements(); ++i) datas(i) = 12.0;
+  EXPECT_TRUE(feature_test_main(
+      /*mlir_file_path*/ c_ft_path +
+          "quantized_conv2d_with_bias_and_requantize_p_i8_i8_i8_no_bias_qint32."
+          "mlir",
+      /*backend_types*/ {BackendType::kAArch64},
+      /*num_inputs*/ 1,
+      /*num_outputs*/ 1,
+      /*input_descriptors*/ {"1x2x2x25xf32_X"},
+      /*output_descriptors*/ {"f32_X"},
+      /*input_vals*/ {inputs},
+      /*expect_output_vals*/ {output}));
+}
+
 }  // namespace mlir_test

dac32188ed3890caa2a78b9dfdb03f59be712dd3
Wenyi Zhao <kevin.zwy@alibaba-inc.com>
2022-06-15 15:07:17 +0800

    support acl conv + depthwise_conv (#392)
    * support acl conv + depthwise_conv
    * support ACL conv directly
    * support ACL depthwise_cnov directly
    * support pre-packing for ACL conv/depthwise_conv/qconv

--- tao_compiler/mlir/xla/ral/context/common_context_impl_mkldnn.h
+++ tao_compiler/mlir/xla/ral/context/common_context_impl_mkldnn.h
@@ -14,6 +14,8 @@
 
 #if defined(TAO_CPU_ONLY) && defined(TAO_ENABLE_MKLDNN)
 
+#include <thread>
+
 #include "dnnl_threadpool_iface.hpp"
 #include "tensorflow/compiler/mlir/xla/ral/context/common_context_impl.h"
 #include "tensorflow/compiler/mlir/xla/ral/context/context_util.h"
@@ -21,6 +23,11 @@
 #include "tensorflow/compiler/mlir/xla/ral/device/cpu/cpu_driver.h"
 #include "tensorflow/compiler/mlir/xla/ral/ral_base.h"
 #include "tensorflow/compiler/mlir/xla/ral/ral_helper.h"
+
+#if defined(TAO_AARCH64)
+#include "arm_compute/core/Types.h"
+#include "arm_compute/runtime/NEON/NEFunctions.h"
+#endif
 
 namespace tao {
 namespace ral {
@@ -38,7 +45,7 @@ DiscCpuMathKernelMode GetDiscCpuMathKernelMode();
 bool promoteConv1DToConv2D();
 
 // Returns true if weight prepacking is enabled.
-bool enableWeightPrePacking();
+bool isWeightPrePackingEnabled();
 
 // Returns the maximum number of copied we can cache.
 int getWeightPrePackingCacheCapacity();
@@ -68,6 +75,14 @@ inline data_type toDataType<int8_t>() {
   return data_type::s8;
 }
 
+template <class T>
+inline void hash_combine(std::size_t& seed, const T& v) {
+  std::hash<T> hasher;
+  seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+extern std::thread::id kDiscCpuDefaultThreadId;
+
 struct ConvParams {
   format_tag input_format;
   format_tag filter_format;
@@ -82,8 +97,80 @@ struct ConvParams {
   dims padding_l;
   dims padding_r;
   int groups;
-  bool weight_is_const;
+  bool weight_is_const = false;
+  bool is_depthwise = false;
 };
+
+struct ConvParamsKey {
+  std::vector<int64_t> src_dims;
+  std::vector<int64_t> weight_dims;
+  std::vector<int64_t> dst_dims;
+  // padding & stride & dilation & groups & weight_is_const ...
+  std::vector<int32_t> metadata;
+  opaque_t weight_ptr = nullptr;
+  // We need this in case the cased kernel is not thead safe.
+  // To enable large parallelism, we cache the primitive per-thread.
+  std::thread::id tid;
+};
+
+inline bool operator==(const ConvParamsKey& lhs, const ConvParamsKey& rhs) {
+  return (lhs.src_dims == rhs.src_dims && lhs.weight_dims == rhs.weight_dims &&
+          lhs.dst_dims == rhs.dst_dims && lhs.metadata == rhs.metadata &&
+          lhs.weight_ptr == rhs.weight_ptr && lhs.tid == rhs.tid);
+}
+
+struct ConvParamsKeyHasher {
+  std::size_t operator()(const ConvParamsKey& key) const {
+    std::size_t seed = std::hash<size_t>()(key.src_dims.size());
+    hash_combine(seed, key.weight_dims.size());
+    hash_combine(seed, key.dst_dims.size());
+    hash_combine(seed, key.metadata.size());
+    hash_combine(seed, key.weight_ptr);
+    hash_combine(seed, key.tid);
+    for (size_t i = 0; i < key.src_dims.size(); ++i) {
+      hash_combine(seed, key.src_dims[i]);
+    }
+    for (size_t i = 0; i < key.weight_dims.size(); ++i) {
+      hash_combine(seed, key.weight_dims[i]);
+    }
+    for (size_t i = 0; i < key.dst_dims.size(); ++i) {
+      hash_combine(seed, key.dst_dims[i]);
+    }
+    for (size_t i = 0; i < key.metadata.size(); ++i) {
+      hash_combine(seed, key.metadata[i]);
+    }
+    return seed;
+  }
+};
+
+template <typename Tinput, int NDims, typename Tfilter = Tinput,
+          typename Toutput = Tinput>
+inline ConvParamsKey makeConvParamsKey(MemRefType<Tinput, NDims> input,
+                                       MemRefType<Tfilter, NDims> kernel,
+                                       MemRefType<int32_t, 1> padding,
+                                       MemRefType<Toutput, NDims> output,
+                                       MemRefType<int32_t, 1> metadata,
+                                       const std::thread::id& tid) {
+  ConvParamsKey key;
+  key.src_dims.reserve(NDims);
+  key.weight_dims.reserve(NDims);
+  key.dst_dims.reserve(NDims);
+  key.metadata.reserve(metadata.sizes[0] + padding.sizes[0]);
+  for (int i = 0; i < NDims; ++i) {
+    key.src_dims.push_back(input.sizes[i]);
+    key.weight_dims.push_back(kernel.sizes[i]);
+    key.dst_dims.push_back(output.sizes[i]);
+  }
+  for (int i = 0; i < metadata.sizes[0]; ++i) {
+    key.metadata.push_back(metadata.data[i]);
+  }
+  for (int i = 0; i < padding.sizes[0]; ++i) {
+    key.metadata.push_back(padding.data[i]);
+  }
+  key.weight_ptr = kernel.data;
+  key.tid = tid;
+  return key;
+}
 
 template <typename Tinput, int N, typename Tfilter = Tinput,
           typename Toutput = Tinput>
@@ -247,6 +334,8 @@ bool parseConvParams(ExecutionContext* ctx, MemRefType<Tinput, N> input,
     TAO_VLOG(0) << "output format: " << format_buffer.data();
   }
 
+  params->is_depthwise = (filter_dims[1] == 1);
+
   if (promote_conv1d_to_conv2d) {
     params->strides.push_back(1);
     params->dilates.push_back(1);
@@ -285,6 +374,7 @@ bool parseConvParams(ExecutionContext* ctx, MemRefType<Tinput, N> input,
       TAO_VLOG(0) << "\t" << output_dims[i];
     }
     TAO_VLOG(0) << "weight_is_const = " << params->weight_is_const;
+    TAO_VLOG(0) << "is_depthwise = " << params->is_depthwise;
   }
 
   data_type input_dtype = toDataType<Tinput>();
@@ -313,6 +403,104 @@ bool parseConvParams(ExecutionContext* ctx, MemRefType<Tinput, N> input,
   params->dst_dims = output_dims;
   return true;
 }
+
+template <typename Tinput, typename Tfilter = Tinput, typename Toutput = Tinput>
+void dumpConvLikeKernelProflingInfo(const ConvParams& params, size_t nanosec,
+                                    const char* message) {
+  const auto& src_dims = params.src.get_dims();
+  const auto& kernel_dims = params.weight.get_dims();
+  const auto& dst_dims = params.dst.get_dims();
+
+  int64_t bytes =
+      static_cast<int64_t>(params.src.get_nelems()) * sizeof(Tinput) +
+      static_cast<int64_t>(params.weight.get_nelems()) * sizeof(Tfilter) +
+      static_cast<int64_t>(params.dst.get_nelems()) * sizeof(Toutput);
+
+  int64_t OC = params.dst.get_dims()[1];
+  // 2 * KH * KW * KIC - 1
+  int64_t flops_per_output =
+      2 * static_cast<int64_t>(params.weight.get_nelems()) / OC - 1;
+  // #outputs * flops_per_output
+  int64_t gflops =
+      static_cast<int64_t>(params.dst.get_nelems()) * flops_per_output;
+
+  std::ostringstream sout;
+  sout << message << ":\n";
+  sout << "  input logical NCHW shape:\n\t";
+  for (const auto& d : src_dims) {
+    sout << d << " ";
+  }
+  sout << "\n  kernel logical OIHW shape:\n\t";
+  for (const auto& d : kernel_dims) {
+    sout << d << " ";
+  }
+  sout << "\n  output logical NCHW shape:\n\t";
+  for (const auto& d : dst_dims) {
+    sout << d << " ";
+  }
+  sout << "\n  strides:\n\t";
+  for (size_t i = 0; i < params.strides.size(); ++i) {
+    sout << params.strides[i] << " ";
+  }
+  sout << "\n  dilates:\n\t";
+  for (size_t i = 0; i < params.dilates.size(); ++i) {
+    sout << params.dilates[i] << " ";
+  }
+  sout << "\n  paddings_l:\n\t";
+  for (size_t i = 0; i < params.padding_l.size(); ++i) {
+    sout << params.padding_l[i] << " ";
+  }
+  sout << "\n  paddings_r:\n\t";
+  for (size_t i = 0; i < params.padding_r.size(); ++i) {
+    sout << params.padding_r[i] << " ";
+  }
+  TAO_VLOG(0) << sout.str() << "\n roofline:\n"
+              << "\tMath Ops = " << gflops << "\n"
+              << "\tBytes = " << bytes << "\n"
+              << "\tBandwidth = " << double(bytes) / double(nanosec) << " GB\n"
+              << "\tGFLOPS = " << double(gflops) / double(nanosec) << "\n";
+}
+
+#if defined(TAO_AARCH64)
+template <typename OpTy>
+struct AclConvLikeInfo {
+  arm_compute::Tensor src;
+  arm_compute::Tensor weights;
+  arm_compute::Tensor dst;
+  OpTy op;
+};
+
+template <typename TKey, typename TValue>
+using AclConvLikeMap = std::unordered_map<TKey, TValue, ConvParamsKeyHasher>;
+template <typename OpTy>
+using AclConvLikeCache = ideep::utils::lru_cache<
+    ConvParamsKey, std::shared_ptr<AclConvLikeInfo<OpTy>>, AclConvLikeMap>;
+
+template <typename OpTy>
+struct AclConvLikeState : public Context::Resource {
+  std::mutex mu;
+  AclConvLikeCache<OpTy> cache{getWeightPrePackingCacheCapacity()};
+
+  inline std::shared_ptr<AclConvLikeInfo<OpTy>> getOrCreate(
+      const ConvParamsKey& key,
+      std::function<std::shared_ptr<AclConvLikeInfo<OpTy>>()> creator) {
+    std::lock_guard<std::mutex> l(mu);
+    auto it = cache.find(key);
+    if (it == cache.end()) {
+      it = cache.insert(std::make_pair(key, creator())).first;
+    }
+    return it->second;
+  }
+};
+
+using AclDepthwiseConvInfo =
+    AclConvLikeInfo<arm_compute::NEDepthwiseConvolutionLayer>;
+using AclDepthwiseConvState =
+    AclConvLikeState<arm_compute::NEDepthwiseConvolutionLayer>;
+using AclConvInfo = AclConvLikeInfo<arm_compute::NEConvolutionLayer>;
+using AclConvState = AclConvLikeState<arm_compute::NEConvolutionLayer>;
+
+#endif  // TAO_AARCH64
 
 }  // namespace ral
 }  // namespace tao

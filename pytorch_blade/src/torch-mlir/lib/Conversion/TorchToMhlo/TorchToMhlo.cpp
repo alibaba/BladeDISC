4c20b8681189ad3eb8007418a45c9f5a72e7b23a
Yancey1989 <yancey.yx@antgroup.com>
2022-06-29 10:45:16 +0800

    update

--- pytorch_blade/src/torch-mlir/lib/Conversion/TorchToMhlo/TorchToMhlo.cpp
+++ pytorch_blade/src/torch-mlir/lib/Conversion/TorchToMhlo/TorchToMhlo.cpp
@@ -785,37 +785,6 @@ LogicalResult ConvertAtenOp<AtenGeluOp>::matchAndRewrite(
   return success();
 }
 
-template <>
-LogicalResult ConvertAtenOp<AtenEmptyMemoryFormatOp>::matchAndRewrite(
-    AtenEmptyMemoryFormatOp op,
-    OpAdaptor adaptor,
-    ConversionPatternRewriter& rewriter) const {
-  Location loc = op.getLoc();
-  SmallVector<int64_t, 4> dshape;
-  if (!matchPattern(op.size(), m_TorchConstantIntList(dshape)))
-    return rewriter.notifyMatchFailure(
-        op, "non-const size parameter unsupported");
-  auto opType = getTypeConverter()
-                    ->convertType(op.getType())
-                    .template cast<RankedTensorType>();
-  if (!opType) {
-    return op.emitError("should be RankedTensorType");
-  }
-  Type resultElementType;
-  if (op.dtype().getType().isa<Torch::NoneType>()) {
-    resultElementType = opType.getElementType();
-  } else {
-    return op.emitError("dtype should be none");
-  }
-
-  if (!resultElementType.isa<mlir::FloatType>()) {
-    return op.emitError("output dtype should be float32");
-  }
-  auto result = mhlo::getConstTensor<float>(rewriter, op, 0.0, dshape);
-  rewriter.replaceOp(op, result.getValue());
-  return success();
-}
-
 using ReductionConvFunc = llvm::Optional<Value> (*)(
     PatternRewriter&,
     Operation*,
@@ -2260,7 +2229,6 @@ class ConvertTorchToMhlo
     INSERT_ATENOP_PATTERN(AtenGeluOp);
     INSERT_ATENOP_PATTERN(AtenSizeIntOp);
     INSERT_ATENOP_PATTERN(AtenTanhOp);
-    INSERT_ATENOP_PATTERN(AtenEmptyMemoryFormatOp);
     // INSERT_ATENOP_PATTERN(AtenArgmaxOp);
     INSERT_ATENOP_PATTERN(AtenPowTensorScalarOp);
     INSERT_ATENOP_PATTERN(AtenRsubScalarOp);
--- pytorch_blade/tests/mhlo/constructors.mlir
+++
@@ -1,9 +0,0 @@
-// RUN: torch-mlir-opt <%s --torch-backend-to-mhlo-backend-pipeline -split-input-file -verify-diagnostics | FileCheck %s
-func @torch.aten.empty() -> !torch.vtensor<[2,3], f32> {
-  %none = torch.constant.none
-  %1 = torch.constant.int 2
-  %2 = torch.constant.int 3
-  %3 = torch.prim.ListConstruct %1, %2 : (!torch.int, !torch.int) -> !torch.list<int>
-  %4 = torch.aten.empty.memory_format %3, %none, %none, %none, %none, %none : !torch.list<int>, !torch.none, !torch.none, !torch.none, !torch.none, !torch.none -> !torch.vtensor<[2,3],f32>
-  return %4 : !torch.vtensor<[2, 3], f32>
-}

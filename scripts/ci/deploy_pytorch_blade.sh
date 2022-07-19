a5b1dfd061adc73fa48dbf4f7832d639664b2976
Tanyo Kwok <tianyou.gty@alibaba-inc.com>
2022-07-19 17:00:42 +0800

    Fix deploy docker torchvision/torch version (#468)

--- scripts/ci/deploy_pytorch_blade.sh
+++ scripts/ci/deploy_pytorch_blade.sh
@@ -18,7 +18,7 @@ source ${SCRIPT_DIR}/deploy_docker.sh
 
 # build runtime Docker
 docker build -t ${REMOTE_RUNTIME_DOCKER} -f docker/runtime/Dockerfile.pytorch \
-  --build-arg BASEIMAGE=${RUNTIME_BASEIMAGE} .
+  --build-arg BASEIMAGE=${RUNTIME_BASEIMAGE} --build-arg TORCH_VERSION=${TORCH_VERSION} .
 
 
 # push TorchBlade runtime Docker images

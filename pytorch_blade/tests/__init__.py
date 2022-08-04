e871f761b9b6530bb8d20d99a8b9be9c6708e54f
Yancey1989 <yancey.yx@antgroup.com>
2022-08-04 11:39:00 +0800

    update

--- pytorch_blade/tests/__init__.py
+++ pytorch_blade/tests/__init__.py
@@ -8,3 +8,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+try:
+    import onnx
+except Exception:
+    # note: import onnx before torch_blade to avoid pb conflict
+    pass

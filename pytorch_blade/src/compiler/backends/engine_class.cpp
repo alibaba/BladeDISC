110517cfb74ad575af8c955e829fdb18e77a5d46
Yancey1989 <yancey.yx@antgroup.com>
2022-09-14 11:36:12 +0800

    fix previous pytorch version

--- pytorch_blade/src/compiler/backends/engine_class.cpp
+++ pytorch_blade/src/compiler/backends/engine_class.cpp
@@ -72,7 +72,11 @@ at::List<at::Tensor> EngineClass::Execute(const at::List<at::Tensor>& inputs) {
         torch::QualifiedName(*module.type()->name(), "forward");
     auto func =
         GetFallback()._ivalue()->compilation_unit()->find_function(method_name);
+#if PYTORCH_MAJOR_VERSION == 1 && PYTORCH_MINOR_VERSION >= 11
     auto graph = torch::jit::tryToGraphFunction(*func)->graph();
+#else
+    auto graph = *func.graph();
+#endif
 
     VLOG(0) << "replay toolkit dump cluster on: " << dump_path
             << " , the sub-graph: \n"

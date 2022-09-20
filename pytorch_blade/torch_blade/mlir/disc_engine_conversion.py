9b690277bfce5b6b8902e74d3849024c8aebdf94
TanyoKwok <tianyou.gty@alibaba-inc.com>
2022-09-20 13:37:24 +0800

    Refine

--- pytorch_blade/torch_blade/mlir/disc_engine_conversion.py
+++ pytorch_blade/torch_blade/mlir/disc_engine_conversion.py
@@ -79,7 +79,7 @@ def _compile_torchscript(graph):
             # RUN: disc_compiler_main input_mlir_file.mlir output_file.so
             # redirect stdout to devnull
             subprocess.check_call(
-                [mhlo_compile_cmd, inp_mlir_file.name, out_file_name, "--multi-cc-support", "--mlir-elide-elementsattrs-if-larger=4"],
+                [mhlo_compile_cmd, inp_mlir_file.name, out_file_name, "--multi-cc-support", "--mlir-elide-elementsattrs-if-larger=8"],
                 stdout=devnull,
                 stderr=devnull,
                 env=env,

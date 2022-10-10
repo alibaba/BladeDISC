3240306a81af69de499a11e2fd1a9688c4e95430
TanyoKwok <tianyou.gty@alibaba-inc.com>
2022-10-10 17:10:36 +0800

    Disable conv ut when torch ver <= 1.6.1
    the schema of aten::_convolution is updated after torch 1.6.1

--- pytorch_blade/tests/disc/ops/test_conv_ops.py
+++ pytorch_blade/tests/disc/ops/test_conv_ops.py
@@ -13,8 +13,9 @@ import torch
 import torch_blade
 import unittest
 
-from tests.disc.testing_base import DiscTestCase
+from tests.disc.testing_base import DiscTestCase, skipTorchLE
 
+@skipTorchLE("1.6.1")
 class TestMlirConvolution(DiscTestCase):
     def _test_conv(self, conv_func, inp_test_data=None):
         if inp_test_data is not None:

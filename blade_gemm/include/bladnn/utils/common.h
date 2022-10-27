7c73442de505728d1d04fb0e512a03924f4421a3
linbinskn <linbinskn@outlook.com>
2022-10-27 15:37:41 +0800

    delete

--- blade_gemm/include/bladnn/utils/common.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace bladnn {
-namespace utils {
-bool startswith(const std::string& str, const std::string& prefix);
-}  // namespace utils
-}  // namespace bladnn

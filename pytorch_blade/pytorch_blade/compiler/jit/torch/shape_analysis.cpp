0e637089c5d3a839c03354c0d97c183fdc9c8b07
Yancey1989 <yancey.yx@antgroup.com>
2022-11-08 17:11:15 +0800

    upate

--- pytorch_blade/pytorch_blade/compiler/jit/torch/shape_analysis.cpp
+++ pytorch_blade/pytorch_blade/compiler/jit/torch/shape_analysis.cpp
@@ -927,8 +927,8 @@ class ShapePropagator : public PropertyPropBase {
             "aten::relu(Tensor self) -> Tensor",
             "aten::relu_(Tensor self) -> Tensor",
             "aten::pow(Tensor self, Scalar exponent) -> Tensor",
-            "aten::zero(Tensor self) -> Tensor",
 #if PYTORCH_MAJOR_VERSION == 1 && PYTORCH_MINOR_VERSION >= 12
+            "aten::zero(Tensor self) -> Tensor",
             "aten::gelu(Tensor self, *, str approximate='none') -> Tensor",
 #endif
 #if PYTORCH_MAJOR_VERSION == 1 && PYTORCH_MINOR_VERSION >= 9

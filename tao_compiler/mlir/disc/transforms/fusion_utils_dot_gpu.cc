6567f4e23ba1a875b3e9a2b60857be420dec2867
ZHENG Zhen <zzchman@gmail.com>
2022-11-10 21:16:29 +0800

    Fix format problem.

--- tao_compiler/mlir/disc/transforms/fusion_utils_dot_gpu.cc
+++ tao_compiler/mlir/disc/transforms/fusion_utils_dot_gpu.cc
@@ -9,9 +9,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "tensorflow/compiler/mlir/disc/transforms/fusion_utils.h"
-
 #include "tensorflow/compiler/mlir/disc/disc_util.h"
+#include "tensorflow/compiler/mlir/disc/transforms/fusion_utils.h"
 #include "tensorflow/compiler/mlir/disc/utils/source_emitter.h"
 
 namespace mlir {

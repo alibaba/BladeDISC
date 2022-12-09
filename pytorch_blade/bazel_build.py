16d44919074790bf0742d99eec263c91f98ca7cb
zhangxiao-stack <1244360827@qq.com>
2022-12-09 12:41:35 +0800

    resolve the conflict

--- pytorch_blade/bazel_build.py
+++ pytorch_blade/bazel_build.py
@@ -16,7 +16,15 @@ import subprocess
 import sys
 import torch
 import venv
-from common_setup import running_on_ci, remote_cache_token, which, num_make_jobs, is_aarch64
+from common_setup import (
+    running_on_ci,
+    remote_cache_token,
+    which,
+    num_make_jobs,
+    is_aarch64,
+    build_tao_compiler_add_flags_platform_alibaba_cached,
+    test_tao_compiler_add_flags_platform_alibaba_cached,
+)
 from torch_blade_build import TorchBladeBuild, get_fullpath_or_create
 
 cwd = os.path.dirname(os.path.abspath(__file__))

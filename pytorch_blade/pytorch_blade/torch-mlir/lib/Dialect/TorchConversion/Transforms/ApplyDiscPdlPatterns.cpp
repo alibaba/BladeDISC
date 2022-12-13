c113646985d5f9d507456537b3c53ea1762ba395
Wenyi Zhao <kevin.zwy@alibaba-inc.com>
2022-12-13 15:05:27 +0800

    [pdl] groupnorm support

--- pytorch_blade/pytorch_blade/torch-mlir/lib/Dialect/TorchConversion/Transforms/ApplyDiscPdlPatterns.cpp
+++ pytorch_blade/pytorch_blade/torch-mlir/lib/Dialect/TorchConversion/Transforms/ApplyDiscPdlPatterns.cpp
@@ -101,6 +101,229 @@ bool isOpTriviallyDeadDisc(Operation* op) {
 // add pre-defined pdll patterns here.
 std::string getTorchPredefinedPDLPatterns() {
   std::string preDefinedPatterns;
+#if defined(PLATFORM_ALIBABA) and defined(ENABLE_BLADE_GEMM)
+  preDefinedPatterns += R"pdll(
+    Rewrite ConvertToF16(value: Value) -> Value {
+      let f16_dtype = op<torch.constant.int> {value = attr<"5">} -> (type<"!torch.int">);
+      let old_type = GetTorchTensorType(value);
+      let new_type = ConvertTorchTensorElemType(old_type, attr<"\"f16\"">);
+      let false_val = op<torch.constant.bool> {value = attr<"false">} -> (type<"!torch.bool">);
+      let none_val = op<torch.constant.none> -> (type<"!torch.none">);
+      let f16_value = op<torch.aten.to.dtype>(
+        value, f16_dtype, false_val, false_val, none_val
+      ) -> (new_type);
+    
+      return f16_value.0;
+    }
+    
+    Rewrite ConvertToF32(value: Value) -> Value {
+      let f32_dtype = op<torch.constant.int> {value = attr<"6">} -> (type<"!torch.int">);
+      let old_type = GetTorchTensorType(value);
+      let new_type = ConvertTorchTensorElemType(old_type, attr<"\"f32\"">);
+      let false_val = op<torch.constant.bool> {value = attr<"false">} -> (type<"!torch.bool">);
+      let none_val = op<torch.constant.none> -> (type<"!torch.none">);
+      let f32_value = op<torch.aten.to.dtype>(
+        value, f32_dtype, false_val, false_val, none_val
+      ) -> (new_type);
+    
+      return f32_value.0;
+    }
+
+    Pattern TorchGroupNormOpF32 {
+      /// match phase: define the pattern
+      let eps_attr : Attr;
+      let eps = op<torch.constant.float> { value = eps_attr };
+      let gn = op<torch.aten.group_norm>(
+        input: Value,
+        num_group: Value,
+        weight: Value,
+        bias: Value,
+        eps.0,
+        cudnn_enabled: Value
+      ) -> (old_type: Type);
+      CheckNotTorchNone(bias);
+      CheckTorchConstantInt(num_group);
+      CheckTorchTensorElemType(input, attr<"\"f32\"">);
+    
+      /// rewrite phase
+      rewrite gn with {
+        let f16_input = ConvertToF16(input);
+        let f16_weight = ConvertToF16(weight);
+        let f16_bias = ConvertToF16(bias);
+        let f16_output = ConvertToF16(gn.0);
+    
+        /// 1. create custom call op
+        let inputs = PackValue_3(attr<"\"in\"">, f16_input, f16_weight, f16_bias);
+        let outputs = PackValue_1(attr<"\"out\"">, f16_output);
+        let infos = CreateTorchCustomCall(attr<"\"op\"">, inputs, outputs);
+    
+        /// 2. set attrs that are used by bladedisc.
+        SetAttr(infos.op, attr<"\"call_target_name\"">, attr<"\"ral_pdll_group_norm\"">);
+        SetAttr(infos.op, attr<"\"input_placements\"">, attr<"\"x,x,x\"">);
+        SetAttr(infos.op, attr<"\"output_placements\"">, attr<"\"x\"">);
+        SetAttr(infos.op, attr<"\"device\"">, attr<"\"x\"">);
+        SetAttr(infos.op, attr<"\"input_layouts\"">, attr<"\"NCHW,*,*\"">);
+        SetAttr(infos.op, attr<"\"output_layouts\"">, attr<"\"NCHW\"">);
+        SetAttr(infos.op, attr<"\"expected_input_layouts\"">, attr<"\"NHWC,*,*\"">);
+        SetAttr(infos.op, attr<"\"expected_output_layouts\"">, attr<"\"NHWC\"">);
+    
+        /// 3. set attrs that are directly passed to the custom call kernel.
+        let num_group_attr = ConvertTorchConstantIntToI64Attr(num_group);
+        SetCustomAttr(infos.op, attr<"\"num_group\"">, num_group_attr);
+        SetCustomAttr(infos.op, attr<"\"eps\"">, eps_attr);
+        SetCustomAttr(infos.op, attr<"\"silu\"">, attr<"false">);
+    
+        let rs = UnpackValue_1(infos.new_outputs);
+        let new_output = ConvertToF32(rs);
+    
+        replace gn with new_output;
+      };
+    }
+
+    Pattern TorchGroupNormWithSiluOpF32 {
+      /// match phase: define the pattern
+      let eps_attr : Attr;
+      let eps = op<torch.constant.float> { value = eps_attr };
+      let gn = op<torch.aten.group_norm>(
+        input: Value,
+        num_group: Value,
+        weight: Value,
+        bias: Value,
+        eps.0,
+        cudnn_enabled: Value
+      ) -> (old_type: Type);
+      let silu = op<torch.aten.silu>(gn.0);
+      CheckNotTorchNone(bias);
+      CheckTorchConstantInt(num_group);
+      CheckTorchTensorElemType(input, attr<"\"f32\"">);
+    
+      /// rewrite phase
+      rewrite silu with {
+        let f16_input = ConvertToF16(input);
+        let f16_weight = ConvertToF16(weight);
+        let f16_bias = ConvertToF16(bias);
+        let f16_output = ConvertToF16(silu.0);
+    
+        /// 1. create custom call op
+        let inputs = PackValue_3(attr<"\"in\"">, f16_input, f16_weight, f16_bias);
+        let outputs = PackValue_1(attr<"\"out\"">, f16_output);
+        let infos = CreateTorchCustomCall(attr<"\"op\"">, inputs, outputs);
+    
+        /// 2. set attrs that are used by bladedisc.
+        SetAttr(infos.op, attr<"\"call_target_name\"">, attr<"\"ral_pdll_group_norm\"">);
+        SetAttr(infos.op, attr<"\"input_placements\"">, attr<"\"x,x,x\"">);
+        SetAttr(infos.op, attr<"\"output_placements\"">, attr<"\"x\"">);
+        SetAttr(infos.op, attr<"\"device\"">, attr<"\"x\"">);
+        SetAttr(infos.op, attr<"\"input_layouts\"">, attr<"\"NCHW,*,*\"">);
+        SetAttr(infos.op, attr<"\"output_layouts\"">, attr<"\"NCHW\"">);
+        SetAttr(infos.op, attr<"\"expected_input_layouts\"">, attr<"\"NHWC,*,*\"">);
+        SetAttr(infos.op, attr<"\"expected_output_layouts\"">, attr<"\"NHWC\"">);
+    
+        /// 3. set attrs that are directly passed to the custom call kernel.
+        let num_group_attr = ConvertTorchConstantIntToI64Attr(num_group);
+        SetCustomAttr(infos.op, attr<"\"num_group\"">, num_group_attr);
+        SetCustomAttr(infos.op, attr<"\"eps\"">, eps_attr);
+        SetCustomAttr(infos.op, attr<"\"silu\"">, attr<"true">);
+    
+        let rs = UnpackValue_1(infos.new_outputs);
+        let new_output = ConvertToF32(rs);
+    
+        replace silu with new_output;
+      };
+    }
+
+    Pattern TorchGroupNormOpF16 {
+      /// match phase: define the pattern
+      let eps_attr : Attr;
+      let eps = op<torch.constant.float> { value = eps_attr };
+      let gn = op<torch.aten.group_norm>(
+        input: Value,
+        num_group: Value,
+        weight: Value,
+        bias: Value,
+        eps.0,
+        cudnn_enabled: Value
+      ) -> (old_type: Type);
+      CheckNotTorchNone(bias);
+      CheckTorchConstantInt(num_group);
+      CheckTorchTensorElemType(input, attr<"\"f16\"">);
+    
+      /// rewrite phase
+      rewrite gn with {
+        /// 1. create custom call op
+        let inputs = PackValue_3(attr<"\"in\"">, input, weight, bias);
+        let outputs = PackValue_1(attr<"\"out\"">, gn.0);
+        let infos = CreateTorchCustomCall(attr<"\"op\"">, inputs, outputs);
+    
+        /// 2. set attrs that are used by bladedisc.
+        SetAttr(infos.op, attr<"\"call_target_name\"">, attr<"\"ral_pdll_group_norm\"">);
+        SetAttr(infos.op, attr<"\"input_placements\"">, attr<"\"x,x,x\"">);
+        SetAttr(infos.op, attr<"\"output_placements\"">, attr<"\"x\"">);
+        SetAttr(infos.op, attr<"\"device\"">, attr<"\"x\"">);
+        SetAttr(infos.op, attr<"\"input_layouts\"">, attr<"\"NCHW,*,*\"">);
+        SetAttr(infos.op, attr<"\"output_layouts\"">, attr<"\"NCHW\"">);
+        SetAttr(infos.op, attr<"\"expected_input_layouts\"">, attr<"\"NHWC,*,*\"">);
+        SetAttr(infos.op, attr<"\"expected_output_layouts\"">, attr<"\"NHWC\"">);
+    
+        /// 3. set attrs that are directly passed to the custom call kernel.
+        let num_group_attr = ConvertTorchConstantIntToI64Attr(num_group);
+        SetCustomAttr(infos.op, attr<"\"num_group\"">, num_group_attr);
+        SetCustomAttr(infos.op, attr<"\"eps\"">, eps_attr);
+        SetCustomAttr(infos.op, attr<"\"silu\"">, attr<"false">);
+    
+        let rs = UnpackValue_1(infos.new_outputs);
+        replace gn with rs;
+      };
+    } 
+
+    Pattern TorchGroupNormWithSiluOpF16 {
+      /// match phase: define the pattern
+      let eps_attr : Attr;
+      let eps = op<torch.constant.float> { value = eps_attr };
+      let gn = op<torch.aten.group_norm>(
+        input: Value,
+        num_group: Value,
+        weight: Value,
+        bias: Value,
+        eps.0,
+        cudnn_enabled: Value
+      ) -> (old_type: Type);
+      let silu = op<torch.aten.silu>(gn.0);
+      CheckNotTorchNone(bias);
+      CheckTorchConstantInt(num_group);
+      CheckTorchTensorElemType(input, attr<"\"f16\"">);
+    
+      /// rewrite phase
+      rewrite silu with {
+        /// 1. create custom call op
+        let inputs = PackValue_3(attr<"\"in\"">, input, weight, bias);
+        let outputs = PackValue_1(attr<"\"out\"">, silu.0);
+        let infos = CreateTorchCustomCall(attr<"\"op\"">, inputs, outputs);
+    
+        /// 2. set attrs that are used by bladedisc.
+        SetAttr(infos.op, attr<"\"call_target_name\"">, attr<"\"ral_pdll_group_norm\"">);
+        SetAttr(infos.op, attr<"\"input_placements\"">, attr<"\"x,x,x\"">);
+        SetAttr(infos.op, attr<"\"output_placements\"">, attr<"\"x\"">);
+        SetAttr(infos.op, attr<"\"device\"">, attr<"\"x\"">);
+        SetAttr(infos.op, attr<"\"input_layouts\"">, attr<"\"NCHW,*,*\"">);
+        SetAttr(infos.op, attr<"\"output_layouts\"">, attr<"\"NCHW\"">);
+        SetAttr(infos.op, attr<"\"expected_input_layouts\"">, attr<"\"NHWC,*,*\"">);
+        SetAttr(infos.op, attr<"\"expected_output_layouts\"">, attr<"\"NHWC\"">);
+    
+        /// 3. set attrs that are directly passed to the custom call kernel.
+        let num_group_attr = ConvertTorchConstantIntToI64Attr(num_group);
+        SetCustomAttr(infos.op, attr<"\"num_group\"">, num_group_attr);
+        SetCustomAttr(infos.op, attr<"\"eps\"">, eps_attr);
+        SetCustomAttr(infos.op, attr<"\"silu\"">, attr<"true">);
+    
+        let rs = UnpackValue_1(infos.new_outputs);
+        replace silu with rs;
+      };
+    }
+    
+  )pdll";
+#endif
+
   return preDefinedPatterns;
 }
 
@@ -149,13 +372,18 @@ void ApplyDiscPdlPatternsPass::runOnOperation() {
   MLIRContext* context = &getContext();
   RewritePatternSet patterns(context);
 
+  auto pdll_include_dirs = mlir::disc_ral::ParseFileString(pdll_include_dirs_); 
+
   (void)mlir::disc_ral::populateDiscPdlPatternsFromString(
-      &patterns, getTorchPredefinedPDLPatterns());
+      &patterns, getTorchPredefinedPDLPatterns(),
+      pdll_include_dirs,
+      torch::kDefaultHelperFunctionDeclarations,
+      torch::registerPredefinedHelperFunctions);
 
   (void)mlir::disc_ral::populateDiscPdlPatternsFromFiles(
       &patterns,
       mlir::disc_ral::ParseFileString(pdll_files_),
-      mlir::disc_ral::ParseFileString(pdll_include_dirs_),
+      pdll_include_dirs,
       torch::kDefaultHelperFunctionDeclarations,
       torch::registerPredefinedHelperFunctions);
 

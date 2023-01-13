fa724ea4bff4ce7d0603f936b0f149221e172f09
minmin.smm <minmin.smm@alibaba-inc.com>
2023-01-13 13:13:25 +0800

    add upsample to whitelist

--- pytorch_blade/pytorch_blade/compiler/mlir/converters/torch_mlir_op_filter.cpp
+++ pytorch_blade/pytorch_blade/compiler/mlir/converters/torch_mlir_op_filter.cpp
@@ -38,6 +38,9 @@ bool IsTorchMlirSupported(const torch::jit::Node& node) {
 // clang-format off
 const std::unordered_set<std::string> &GetTorchMlirWhiteList() {
   static std::unordered_set<std::string> white_list{
+#if defined(PLATFORM_ALIBABA) and defined(ENABLE_BLADE_GEMM)
+      "ten::upsample_nearest2d",
+#endif
       "aten::_autocast_to_reduced_precision",
       "aten::_autocast_to_full_precision",
       "aten::__and__",

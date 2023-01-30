4adbcec052b02ec40d473043cba76967c43a9a6f
Wenyi Zhao <kevin.zwy@alibaba-inc.com>
2023-01-30 16:05:43 +0800

    [transform] enhance fusion pass to support epilogue fusion

--- tao_compiler/mlir/disc/transforms/fusion_utils_transform_based.cc
+++ tao_compiler/mlir/disc/transforms/fusion_utils_transform_based.cc
@@ -40,8 +40,44 @@ bool isSupportedDot(Operation* op) {
   return (lhsCntractingDims.size() == 1 && rhsCntractingDims.size() == 1);
 }
 
+bool isScalarConstOp(Operation* op) {
+  auto constOp = dyn_cast<lmhlo::ConstantOp>(op);
+  if (!constOp) return false;
+  MemRefType type = constOp.getOutput().getType().cast<MemRefType>();
+  return (type.getRank() == 0 || constOp.getValue().isSplat());
+}
+
+bool isBcastOp(Operation* op) {
+  return isa<lmhlo::BroadcastInDimOp, lmhlo::BroadcastOp,
+             lmhlo::DynamicBroadcastInDimOp>(op);
+}
+
+bool isSupportedBcast(Operation* op, ShapeAnalysis& shapeAnalysisBase) {
+  if (!isBcastOp(op)) return false;
+  auto shapeIRAnalysis =
+      dynamic_cast<ShapeConstraintIRAnalysis*>(&shapeAnalysisBase);
+  auto dimAttr = op->getAttrOfType<DenseElementsAttr>("broadcast_dimensions");
+  assert(dimAttr);
+  auto dimensions = dimAttr.getValues<int64_t>();
+  auto in = op->getOperand(0);
+  auto inType = in.getType().cast<MemRefType>();
+  auto out = cast<lmhlo::LmhloOp>(op).getResultBuffer();
+  if (inType.getRank() != dimensions.size()) return false;
+  for (auto [inDimIdx, inDimSize] : llvm::enumerate(inType.getShape())) {
+    int64_t outDimIdx = dimensions[inDimIdx];
+    if (inDimSize != ShapedType::kDynamicSize) continue;
+    // linalg generic op does not support "runtime broadcast semantic", thus we
+    // have to know if we need to broadcast in the compile time.
+    if (!shapeIRAnalysis ||
+        !shapeIRAnalysis->isProductEqual(in, {inDimIdx}, out, {outDimIdx}))
+      return false;
+  }
+  return true;
+}
+
 bool TransformBasedCpuFusionStrategy::isFusible(Operation* op) {
-  return isSupportedDot(op) || isa<lmhlo::ConstantOp>(op);
+  return isSupportedDot(op) || isElementWise(op) || isBcastOp(op) ||
+         isa<lmhlo::ConstantOp>(op);
 }
 
 bool TransformBasedCpuFusionStrategy::initFusionPattern(
@@ -53,9 +89,11 @@ bool TransformBasedCpuFusionStrategy::initFusionPattern(
   // special case for single operation.
   if (fusionPattern.getOpList().size() == 1) {
     Operation* op = *fusionPattern.getOpList().begin();
-    if (this->isFusible(op)) {
+    if (!isBcastOp(op) && this->isFusible(op) ||
+        isBcastOp(op) && isSupportedBcast(op, shapeAnalysis)) {
       fusionPattern.setDominantOp(op);
-      fusionPattern.setFusionType(FusionType::kTransform);
+      fusionPattern.setFusionType(isSupportedDot(op) ? FusionType::kTransform
+                                                     : FusionType::kLoop);
     }
     return true;
   }
@@ -64,7 +102,9 @@ bool TransformBasedCpuFusionStrategy::initFusionPattern(
   DenseSet<Operation*> supportedDotOps;
   for (Operation* op : fusionPattern.getOpList()) {
     // early return for the case where there are non supported ops.
-    if (!this->isFusible(op)) return true;
+    if (!this->isFusible(op) ||
+        isBcastOp(op) && !isSupportedBcast(op, shapeAnalysis))
+      return true;
     if (isSupportedDot(op)) {
       supportedDotOps.insert(op);
       dotWeights.insert(op->getOperand(1));
@@ -73,18 +113,44 @@ bool TransformBasedCpuFusionStrategy::initFusionPattern(
 
   // Only support one gemm a.t.m.
   if (supportedDotOps.size() != 1) return true;
+  Operation* dominantDotOp = *supportedDotOps.begin();
 
-  // Only support fuse const ops that are used as weights for some dot ops and
-  // not consumed by ops outside the fusion pattern.
+  // Only support fuse const ops that are not consumed by ops outside the fusion
+  // pattern and have one of the following properties:
+  // - const ops that are used as weights for some dot ops
+  // - const op has single element.
+  DenseSet<Value> constDotWeights;
   for (Operation* op : fusionPattern.getOpList()) {
     if (!isa<lmhlo::ConstantOp>(op)) continue;
-    if (llvm::find(dotWeights, op->getOperand(0)) == dotWeights.end() ||
-        llvm::find(fusionPattern.getRootOps(), op) !=
-            fusionPattern.getRootOps().end())
+    if (llvm::find(fusionPattern.getRootOps(), op) !=
+        fusionPattern.getRootOps().end())
+      return true;
+    if (llvm::find(dotWeights, op->getOperand(0)) != dotWeights.end()) {
+      constDotWeights.insert(op->getOperand(0));
+      continue;
+    }
+    if (!isScalarConstOp(op)) return true;
+  }
+
+  // We only support epilogue fusion right now.
+  // Check if the dot op does not consume any result produced by other
+  // lmhlo ops (except const ops).
+  for (Value operand : dominantDotOp->getOperands().drop_back()) {
+    if (llvm::find(constDotWeights, operand) != constDotWeights.end()) continue;
+    auto& operands = fusionPattern.getOperands();
+    if (llvm::find(operands, operand) == operands.end()) return true;
+  }
+
+  // We only support single output right now
+  if (fusionPattern.getResults().size() != 1) return true;
+  // the shape of the output should be the same as the shape of result of
+  // dominant op.
+  for (Value result : fusionPattern.getResults()) {
+    if (!shapeAnalysis.isShapeEqual(result, dominantDotOp->getOperand(2)))
       return true;
   }
 
-  fusionPattern.setDominantOp(*supportedDotOps.begin());
+  fusionPattern.setDominantOp(dominantDotOp);
   fusionPattern.setFusionType(FusionType::kTransform);
   return true;
 }

2e45ddd86c78f22d96a8f2f4d31b9114bf8daedd
zzp_miracle <zhangzipeng.zzp@alibaba-inc.com>
2023-02-06 11:39:33 +0800

    [TorchBench] modify some torchbench configs (#992)

--- pytorch_blade/benchmark/TorchBench/test_torch_bench.sh
+++ pytorch_blade/benchmark/TorchBench/test_torch_bench.sh
@@ -10,6 +10,7 @@
 # limitations under the License.
 
 # !/bin/bash
+date_str=$(date '+%Y%m%d-%H')
 if [ -f $HOME/.cache/proxy_config ]; then
   source $HOME/.cache/proxy_config
 fi
@@ -36,7 +37,6 @@ python3 -m virtualenv venv --system-site-packages && source venv/bin/activate
 # install dependencies
 python3 -m pip install -q -r $script_dir/requirements_$HARDWARE.txt
 git pull && git checkout main  && git submodule update --init --recursive --depth 1 && python3 install.py --continue_on_fail
-
 pushd $script_dir # pytorch_blade/benchmark/TorchBench
 ln -s $benchmark_repo_dir torchbenchmark
 
@@ -73,7 +73,6 @@ else
 fi
 
 # results
-date_str=$(date '+%Y%m%d-%H')
 oss_link=https://bladedisc-ci.oss-cn-hongkong.aliyuncs.com
 oss_dir=oss://bladedisc-ci/TorchBench/${bench_target}/${date_str}
 OSSUTIL=ossutil
@@ -87,6 +86,9 @@ for result in ${results[@]}
 do
     cat ${result}/summary.csv
     curl ${oss_link}/TorchBench/baseline/${result}_${bench_target}.csv -o $result.csv
+    tar -zcf ${script_dir}/${result}.tar.gz ${result}
+    rm -rf ${result}/profiling
+    /disc/scripts/ci/$OSSUTIL cp ${script_dir}/${result}.tar.gz ${oss_dir}/
     /disc/scripts/ci/$OSSUTIL cp -r ${script_dir}/${result} ${oss_dir}/${result}
 done
 

72e5fe176bce3de59e7d0c8d02f7f0e0c2960ac9
zzp_miracle <zhangzipeng.zzp@alibaba-inc.com>
2023-02-09 13:46:52 +0800

    [Torch-MLIR] add support for clip model (#993)

--- pytorch_blade/tests/disc/ops/test_factory_like.py
+++ pytorch_blade/tests/disc/ops/test_factory_like.py
@@ -18,7 +18,7 @@ from tests.disc.testing_base import DiscTestCase
 
 class TestFactoryLikes(DiscTestCase):
 
-    def _test_factory_like(self, func, d0: int, d1: int, d_or_val: int):
+    def _test_factory_like(self, func, d0: int, d1: int, d_or_val: int, rtol: float=1e-6, atol: float=1e-3):
         class TestModel(torch.nn.Module):
 
             def __init__(self, device):
@@ -29,7 +29,7 @@ class TestFactoryLikes(DiscTestCase):
                 return func(d0, d1, d_or_val, device=self.device)
 
         model = torch.jit.script(TestModel(self.device))
-        self._test_cvt_to_disc(model, (d0, d1, d_or_val))
+        self._test_cvt_to_disc(model, (d0, d1, d_or_val), rtol=rtol, atol=atol)
 
     def test_factory_likes(self):
         self._test_factory_like(torch.zeros, 2, 2, 3)
@@ -49,7 +49,22 @@ class TestFactoryLikes(DiscTestCase):
 
         self._test_factory_like(fulls, 2, 2, 3)
         self._test_factory_like(fulls_dtype, 2, 2, 3)
+        
+        if utils.torch_version_number() <= utils.parse_version("1.8.1"):
+            return
 
+        @torch.jit.script
+        def empty(d0: int, d1: int, val: int, device: torch.device):
+            return torch.empty([d0, d1], device=device)
+
+        @torch.jit.script
+        def empty_dtype(d0: int, d1: int, val: int, device: torch.device):
+            return torch.empty([d0, d1],
+                              dtype=torch.float32, device=device)
+
+        # results are not numeric checkable
+        self._test_factory_like(empty, 2, 2, 3, rtol=1e+6, atol=1e+6)
+        self._test_factory_like(empty_dtype, 2, 2, 3, rtol=1e+6, atol=1e+6)
 
 if __name__ == "__main__":
     unittest.main()

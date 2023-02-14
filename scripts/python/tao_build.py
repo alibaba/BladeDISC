80a588e100c5c53c5bc7994f2aabc4cf397946ff
Peilin Guo <guopeilin.gpl@alibaba-inc.com>
2023-02-14 17:12:21 +0800

    fix

--- scripts/python/tao_build.py
+++ scripts/python/tao_build.py
@@ -587,7 +587,6 @@ def prepare_env(args):
     os.environ["TAO_BUILD_GIT_HEAD"] = str(git_head())
     host = socket.gethostname()
     os.environ["TAO_BUILD_HOST"] = host
-    print("GPL "+host)
     os.environ["TAO_BUILD_IP"] = socket.gethostbyname(host)
     timestamp = datetime.today().strftime("%Y%m%d%H%M%S")
     os.environ["TAO_BUILD_TIME"] = timestamp

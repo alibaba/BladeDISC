a256de0008e35a8069087b77e44889d8c231a20e
chenbohua3 <chen_bohua@hotmail.com>
2023-02-24 15:40:27 +0800

    add copyright

--- examples/PyTorch/Quantization/bert_ptq.py
+++ examples/PyTorch/Quantization/bert_ptq.py
@@ -1,3 +1,14 @@
+# Copyright 2023 The BladeDISC Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import contextlib
 import os
 import time
@@ -85,7 +96,7 @@ def calibrate(model, dataloader, device):
 
 
 def get_dummy_input():
-    batch = 1
+    batch = 6
     seq_len = 128
     input_ids = torch.randint(
         100, 1000, (batch, seq_len), dtype=torch.int64, device="cpu")

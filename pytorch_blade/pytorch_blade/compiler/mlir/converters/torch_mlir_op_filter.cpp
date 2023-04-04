2fbb9fe33efe6155e71c8aa60c4c6ea9d31b4490
Yan Xu <yancey1989@gmail.com>
2023-04-04 19:15:30 +0800

    polish code

--- pytorch_blade/pytorch_blade/compiler/mlir/converters/torch_mlir_op_filter.cpp
+++ pytorch_blade/pytorch_blade/compiler/mlir/converters/torch_mlir_op_filter.cpp
@@ -168,28 +168,17 @@ const std::unordered_set<std::string> &GetTorchMlirWhiteList() {
       "torch_blade::fake_quant"
     };
 
-  auto readListFromEnvString = [](const std::string& env_name) -> std::unordered_set<std::string> {
-    auto custom_ops = env::ReadStringFromEnvVar(env_name.c_str(), "");
-    std::istringstream f(custom_ops);
-    std::string s;
-    std::unordered_set<std::string> ret;
-    for (auto s : StrSplit(custom_ops, ';')) {
-      ret.insert(std::string(s));
-    }
-    return ret;
-  };
-
   static std::once_flag white, black;
+
   std::call_once(white, [&]() {
-    std::ostringstream ostr;
-    for (auto s : readListFromEnvString("TORCH_MHLO_OP_WHITE_LIST")) {
+    auto list = StrSplit(env::ReadStringFromEnvVar("TORCH_MHLO_OP_WHITE_LIST", ""), ';');
+    for (auto s : list) {
       white_list.insert(s);
     }
   });
-
   std::call_once(black, [&]() {
-    std::ostringstream ostr;
-    for (auto s : readListFromEnvString("TORCH_MHLO_OP_BLACK_LIST")) {
+    auto list = StrSplit(env::ReadStringFromEnvVar("TORCH_MHLO_OP_BLACK_LIST", ""), ';');
+    for (auto s : list) {
       white_list.erase(s);
     }
   });

acde8b25b31694e8b59f136d262259a119db3f15
zzp_miracle <zhangzipeng.zzp@alibaba-inc.com>
2023-04-04 16:46:11 +0800

    [TorchBlade] add config to not freeze module (#1099)

--- pytorch_blade/pytorch_blade/compiler/jit/torch/shape_analysis.cpp
+++ pytorch_blade/pytorch_blade/compiler/jit/torch/shape_analysis.cpp
@@ -780,9 +780,6 @@ class ShapePropagator : public PropertyPropBase {
         return;
       }
       case prim::GetAttr: {
-        auto cls = node->input()->type()->expect<ClassType>();
-        // propagate any type specializations encoded in the type of the class
-        node->output()->setType(cls->getAttribute(node->s(attr::name)));
         return;
       }
       case aten::_unwrap_optional: {

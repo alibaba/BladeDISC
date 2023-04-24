2bca49b89fff8ae80fd2810d4691f5df2dd6b2f5
Yan Xu <yancey1989@gmail.com>
2023-04-24 16:00:48 +0800

    update

--- tao_compiler/mlir/disc/transforms/disc_lhlo_rewriter.cc
+++ tao_compiler/mlir/disc/transforms/disc_lhlo_rewriter.cc
@@ -1,17 +1,13 @@
-/* Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+// Copyright 2021 The BladeDISC Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // This file implements logic for lowering HLO DISC dialect to LHLO DISC
 // dialect.
@@ -61,9 +57,9 @@ namespace {
 template <typename T>
 using BaseOpConversion = OpConversionPattern<T>;
 
-struct HloToLhloConcatenateOpConverter
+struct LhloConcatenateOpConverter
     : public OpRewritePattern<lmhlo::ConcatenateOp> {
-  explicit HloToLhloConcatenateOpConverter(MLIRContext* context)
+  explicit LhloConcatenateOpConverter(MLIRContext* context)
       : OpRewritePattern(context) {}
 
   bool isFixedShape(lmhlo::ConcatenateOp op) const {
@@ -84,6 +80,9 @@ struct HloToLhloConcatenateOpConverter
     if (!isFixedShape(lhloOp)) return failure();
 
     auto operands = op->getOperands();
+
+    // TODO(yancey): support CPU place
+    if (!placement_utils::isGpuMemRef(operands[0])) return failure();
     int num_input_operands = op->getNumOperands() - 1;
 
     SmallVector<Value, 4> ptr_array;
@@ -140,7 +139,7 @@ struct DiscLhloRewriterPass
                            tensor::TensorDialect>();
     target.addIllegalOp<lmhlo::ConcatenateOp>();
 
-    patterns.insert<HloToLhloConcatenateOpConverter>(&context);
+    patterns.insert<LhloConcatenateOpConverter>(&context);
     if (failed(
             applyPatternsAndFoldGreedily(getOperation(), std::move(patterns))))
       signalPassFailure();

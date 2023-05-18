1c2c546e0bfadda135d58121221c80fe6224d697
Xiafei Qiu <xiafei.qiuxf@alibaba-inc.com>
2023-05-18 20:24:42 +0800

    Allow change disc engine device after loading. (#1146)
    Allow change disc engine device after loading. But deny it during inference.

--- pytorch_blade/tests/disc/test_disc_engine.py
+++ pytorch_blade/tests/disc/test_disc_engine.py
@@ -11,7 +11,8 @@
 
 import unittest
 import torch
-from torch_blade import mlir
+import tempfile
+from torch_blade import mlir, optimize, Config
 
 from tests.disc.testing_base import DiscTestCase
 
@@ -37,6 +38,72 @@ class TestDiscEngine(DiscTestCase):
         self.assertEqual(six, 3 * two)
 
         self.assertEqual(three, 3 * one)
+
+
+class Dummy(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.w = torch.nn.Parameter(torch.randn(10, 10))
+        self.a = torch.nn.ReLU()
+        self.b = torch.nn.ReLU()
+        self.c = torch.nn.ReLU()
+        self.d = torch.nn.ReLU()
+        self.e = torch.nn.ReLU()
+
+    def forward(self, x):
+        x = torch.matmul(x, self.w)
+        x = self.a(x)
+        x = self.b(x)
+        x = self.c(x)
+        x = self.d(x)
+        x = self.e(x)
+        return x
+
+
+@unittest.skipIf(
+    not torch.cuda.is_available() or torch.cuda.device_count() < 2, 
+    "Multi GPUs not available"
+)
+class TestDiscEngineDevice(unittest.TestCase):
+    def setUp(self):
+        self.old_device = torch.cuda.current_device()
+        self.input = torch.randn(10).cuda()
+        model = Dummy().eval().cuda()
+        self.ref_output = model(self.input)
+        traced = torch.jit.trace(model, self.input)
+        with Config():
+            pt_model = optimize(traced, model_inputs=(self.input))
+        self.model_f = tempfile.NamedTemporaryFile()
+        torch.jit.save(pt_model, self.model_f)
+        self.model_f.seek(0)
+
+    def tearDown(self):
+        self.model_f.close()
+        torch.cuda.set_device(self.old_device)
+
+    def test_change_device_before_infer(self):
+        # Load in one device
+        torch.cuda.set_device(0)
+        model = torch.jit.load(self.model_f)
+        # Infer in another device
+        torch.cuda.set_device(1)
+        output = model(self.input.cuda(1))
+        self.assertTrue(torch.allclose(output.cpu(), self.ref_output.cpu()))
+        # Infer again
+        output = model(self.input.cuda(1))
+        self.assertTrue(torch.allclose(output.cpu(), self.ref_output.cpu()))
+
+    def test_change_device_during_infer(self):
+        # Load in one device
+        torch.cuda.set_device(0)
+        model = torch.jit.load(self.model_f)
+        # Infer once
+        _ = model(self.input.cuda(0))
+        torch.cuda.set_device(1)
+        # Infer in another device
+        with self.assertRaisesRegex(RuntimeError,
+                                    "Device changed during inference\. .*"):
+            _ = model(self.input.cuda(1))
 
 
 if __name__ == "__main__":

e868392bd0bead841d23cb06e1254b88e4824e27
Yan Xu <yancey1989@gmail.com>
2023-07-20 18:26:54 +0800

    fix ci

--- tao_compiler/mlir/disc/transforms/codegen_utils.cc
+++ tao_compiler/mlir/disc/transforms/codegen_utils.cc
@@ -104,7 +104,8 @@ Value emitNumElementsComputation(OpBuilder& b, Location loc, Operation* op) {
   // only const rank is supported for now
   assert(op->getDialect()->getNamespace() == "lmhlo");
   int num_operands = op->getNumOperands();
-  if (isa<lmhlo::DynamicUpdateSliceOp>(op) && isInplaceOperator(op)) {
+  if (isa<lmhlo::DynamicUpdateSliceOp>(op) &&
+      op->getOperand(0) == op->getOperand(num_operands - 1)) {
     return emitNumElementsComputation(b, loc, op->getOperand(1));
   }
   Value result_memref = op->getOperand(num_operands - 1);
